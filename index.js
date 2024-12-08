33728d44f5a36895c95b484682d75d2cb24ec8e7
HaTheGamerz <mostwantedhashir+1@gmail.com>
2024-12-08 19:33:02 +0500

    finish

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+const mongoose = require('mongoose');
+const eventHandler = require('./handlers/eventHandler');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+let status = }
+  },
+  {
+    name: 'Custom Status 1',
+  },
+  {
+    name: 'Custom Status 2',
+    type: ActivityType.Watching,
+  },
+  {
+    name: 'Custom Status 3',
+    type: ActivityType.Listening,
+  ],
+});
+
+(async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.MONGODB_URI, { keepAlive: true });
+    console.log('Connected to DB.');
+
+    eventHandler(client);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+})();
+
+eventHandler(client);
+
+client.login(process.env.TOKEN);
